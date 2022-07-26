f5868f105e74706c5ea4910ae4ad69c6b788ac01
Eduardo Mannarino <eduardo.mannarino@gmail.com>
2022-07-25 22:02:12 -0300

    cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 import currencyRouter from "./routes/currency.js"
 import priceRouter from "./routes/price.js"
 import orderRouter from "./routes/order.js"
@@ -6,6 +7,7 @@ import userRouter from "./routes/user.js"
 
 const app = express()
 
+app.use(cors({origin: "*"}))
 app.use(express.json())
 app.use("/", currencyRouter)
 app.use("/", priceRouter)
