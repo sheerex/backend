41f7d1ea537d1068319938b71d4dedf0969c25e4
Eduardo Mannarino <eduardo.mannarino@gmail.com>
2022-09-23 23:34:20 -0300

    Add DB_PORT

--- database/database.js
+++ database/database.js
@@ -8,10 +8,11 @@ const DB_NAME = process.env?.TEST === "SI" ? process.env.TESTDB_NAME : process.e
 const DB_USER = process.env?.TEST === "SI" ? process.env.TESTDB_USER : process.env.DB_USER
 const DB_PASS = process.env?.TEST === "SI" ? process.env.TESTDB_PASS : process.env.DB_PASS
 const DB_HOST = process.env?.TEST === "SI" ? process.env.TESTDB_HOST : process.env.DB_HOST
+const DB_PORT = process.env?.TEST === "SI" ? process.env.TESTDB_PORT : process.env.DB_PORT
 
 const ssl = DB_SSL === "SI" ? {ssl: {require: true, rejectUnauthorized: false}} : {}
 
-console.log("Connecting to DB:", DB_NAME)
+console.log("Connecting to DB:", DB_NAME, "on:", DB_HOST)
 
 export const database = new Sequelize(
   DB_NAME, 
@@ -19,7 +20,8 @@ export const database = new Sequelize(
   DB_PASS, 
   {
     host:  DB_HOST, 
+    port: DB_PORT,
     dialect: 'postgres',
-    dialectOptions: ssl
+    dialectOptions: ssl,
   }
 )
